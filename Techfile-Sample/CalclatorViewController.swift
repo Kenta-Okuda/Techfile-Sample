08f034b6c9f24896d49429b0d470e8876b3ad53b
Kenta Okuda <kenta19940520@gmail.com>
2016-07-13 04:15:08 +0900

    [add] A04-電卓の実装

--- Techfile-Sample/CalclatorViewController.swift
+++ Techfile-Sample/CalclatorViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class CalclatorViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    var number: Int = 0
+    var number2: Int = 0
+    var oparation: Int = 0
+    var push_btn: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +27,78 @@ class CalclatorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func initCalc() {
+        number = 0
+        number2 = 0
+        oparation = 0
+        push_btn = 0
+        label.text = "0"
+    }
+    
+    func disp(num:Int) {
+        number = number * 10
+        number += num
+        label.text = String(number)
+    }
+    
+    @IBAction func pushButton(sender: UIButton) {
+        push_btn = Int((sender.titleLabel!.text)!)!
+        disp(push_btn)
+    }
+
+    @IBAction func plus() {
+        label.text = "0"
+        oparation = 1
+        number2 = number
+        number = 0
+    }
+    
+    @IBAction func minus() {
+        label.text = "0"
+        oparation = 2
+        number2 = number
+        number = 0
+    }
+    
+    @IBAction func waru() {
+        label.text = "0"
+        oparation = 3
+        number2 = number
+        number = 0
+    }
+    
+    @IBAction func kakeru() {
+        label.text = "0"
+        oparation = 4
+        number2 = number
+        number = 0
+    }
+    
+    @IBAction func equal() {
+        
+        switch(oparation) {
+        case 1:
+            label.text = String(number2 + number)
+            number = number2 + number
+        case 2:
+            label.text = String(number2 - number)
+            number = number2 - number
+        case 3:
+            label.text = String(number2 / number)
+            number = number2 / number
+        case 4:
+            label.text = String(number2 * number)
+            number = number2 * number
+        default:
+            label.text = "Error"
+            initCalc()
+        }
+    }
+    
+    @IBAction func clear() {
+        initCalc()
+    }
+
     /*
     // MARK: - Navigation
 
