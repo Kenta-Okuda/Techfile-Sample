2f067ee302ba3249554cac1655808708a6dabe50
Kenta Okuda <kenta19940520@gmail.com>
2016-07-13 02:01:35 +0900

    Initial Commit

---
+++ Techfile-SampleTests/Techfile_SampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Techfile_SampleTests.swift
+//  Techfile-SampleTests
+//
+//  Created by Kenta Okuda on 7/13/16.
+//  Copyright © 2016 Kenta Okuda. All rights reserved.
+//
+
+import XCTest
+@testable import Techfile_Sample
+
+class Techfile_SampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
